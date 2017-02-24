bd17bc2082548efcb41985db752f065945b7d2cc
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2017-02-24 13:59:04 +0100

    Modified metadata recipe

--- chef/cookbooks/oscm/metadata.rb
+++ chef/cookbooks/oscm/metadata.rb
@@ -6,7 +6,6 @@ description "Installs/Configures OSCM"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version "0.1"
 
-depends "apache2"
 depends "database"
 depends "keystone"
 depends "crowbar-openstack"
